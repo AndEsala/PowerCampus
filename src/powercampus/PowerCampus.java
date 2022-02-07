7f8d77a2298c5325a5f03b63d40b4b596b987b00
AEsala <aesala@uninorte.edu.co>
2022-02-07 14:31:21 -0500

    Base para proyecto: PowerCampus 2

---
+++ src/powercampus/PowerCampus.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package powercampus;
+
+/**
+ *
+ * @author AndEsala
+ */
+public class PowerCampus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
